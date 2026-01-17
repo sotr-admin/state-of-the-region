b1b5e5ff7ed026b3039278c1a03c51d8e2ff6f67
sotr-admin <sotr.github@gmail.com>
2026-01-18 00:11:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const buildPath = path.join(__dirname, "frontend", "build");
+const buildPath = path.join(__dirname, "build");
 
 app.use(express.static(buildPath));
 
+// SPA fallback
 app.get("*", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.listen(port, () => console.log(`Server running on ${port}`));
