97e099ec1996c9da1455cebfa67d6da83d94f17f
sotr-admin <sotr.github@gmail.com>
2026-01-27 14:29:25 -0500

    Update main.py - cors issue

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_origins=[
         #"http://localhost:3000",
         #"http://127.0.0.1:3000",
-        "*"
+        "https://sotr-frontend-web-h6f9fbafakg6axdn.eastus-01.azurewebsites.net"
     ],
     allow_credentials=True,
     allow_methods=["*"],
